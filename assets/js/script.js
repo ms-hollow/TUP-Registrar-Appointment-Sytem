8c0cc88c2ebc9ef4c58e3c39fb69bf1f3a294707
ms-hollow <leonen.abbegail@gmail.com>
2024-01-22 16:20:35 +0800

    added other files and functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,27 @@
-let menu = document.querySelector('#menu-icon');
-let nav_bar = document.querySelector('.nav_bar');
+document.addEventListener('DOMContentLoaded', function () {
 
-menu.onclick = () => {
-    menu.classList.toggle('bx-x');
-    nav_bar.classList.toggle('open');
-}
+    let menu = document.querySelector('#menu-icon');
+    let nav_bar = document.querySelector('.nav_bar');
+
+    if (menu && nav_bar) {
+        menu.onclick = () => {
+            menu.classList.toggle('bx-x');
+            nav_bar.classList.toggle('open');
+        };
+    }
+
+    let sidebarMenuBtn = document.getElementById('menu-btn');
+    let sideBar = document.querySelector('.sidebar');
+
+    if (sidebarMenuBtn) {
+        sidebarMenuBtn.addEventListener('click', function () {
+            sideBar.classList.toggle('active');
+            document.body.classList.toggle('sidebar-active');
+        });
+    } else {
+        console.error("Element with ID 'menu-btn' not found.");
+    }
+
+
+
+});
