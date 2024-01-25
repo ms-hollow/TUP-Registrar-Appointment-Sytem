19e5e1c49bbf2305f5d75cbd4ec6e2487bbee8ed
ms-hollow <leonen.abbegail@gmail.com>
2024-01-25 14:48:28 +0800

    added profile and book confirmation

---
+++ book-appointment/script.js
@@ -0,0 +1,48 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    let menu = document.querySelector('#menu-icon');
+    let nav_bar = document.querySelector('.nav_bar');
+
+    if (menu && nav_bar) {
+        menu.onclick = () => {
+            menu.classList.toggle('bx-x');
+            nav_bar.classList.toggle('open');
+        };
+    }
+
+    let sidebarMenuBtn = document.getElementById('menu-btn');
+    let sideBar = document.querySelector('.sidebar');
+
+    if (sidebarMenuBtn) {
+        sidebarMenuBtn.addEventListener('click', function () {
+            sideBar.classList.toggle('active');
+            document.body.classList.toggle('sidebar-active');
+
+            let h1 = document.querySelector('.main-view .container h1');
+            if (h1) {
+                h1.classList.toggle('hide-h1', sideBar.classList.contains('active'));
+            }
+        });
+    } else {
+        console.error("Element with ID 'menu-btn' not found.");
+    }
+
+    let footer = document.querySelector('.footer');
+    
+        function toggleFooterVisibility() {
+          // Check if the user has scrolled to the bottom
+          if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+            footer.style.visibility = 'visible';
+          } else {
+            footer.style.visibility = 'hidden';
+          }
+        }
+    
+        // Initial check
+        toggleFooterVisibility();
+    
+        // Listen for scroll events
+        window.addEventListener('scroll', toggleFooterVisibility);
+
+
+});
