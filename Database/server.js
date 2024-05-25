252f8952c398770bef7954e8e07f5127a2e05e6d
pauloqt <jpaulo.alcantara329@gmail.com>
2024-05-25 17:31:54 +0800

    fic index.html redirection

--- Database/server.js
+++ Database/server.js
@@ -1,31 +1,35 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');  // Add this line
+const cors = require('cors');
 const db = require('./database');
+const path = require('path');
 
 const app = express();
 const PORT = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());  // Add this line
+app.use(cors());
 
-app.use(express.static('../General'));
+// Serve static files from the parent directory
+app.use(express.static(path.join(__dirname, '..')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../General/index.html'));
+});
 
 app.post('/register', (req, res) => {
-    const { firstName, middleName, lastName, sex, birthDate, age, contactNumber, address, studentType, course, yearSection, tupID, password } = req.body;
+  const { firstName, middleName, lastName, sex, birthDate, age, contactNumber, address, studentType, course, yearSection, tupID, password } = req.body;
+  const sql = `INSERT INTO users (firstName, middleName, lastName, sex, birthDate, age, contactNumber, address, studentType, course, yearSection, tupID, password) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
 
-    const sql = `INSERT INTO users (firstName, middleName, lastName, sex, birthDate, age, contactNumber, address, studentType, course, yearSection, tupID, password)
-                 VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
-
-    db.run(sql, [firstName, middleName, lastName, sex, birthDate, age, contactNumber, address, studentType, course, yearSection, tupID, password], function (err) {
-        if (err) {
-            return res.status(500).send('Failed to register user');
-        }
-        // Redirect to index.html after successful registration
-        res.redirect('/index.html');
-    });
+  db.run(sql, [firstName, middleName, lastName, sex, birthDate, age, contactNumber, address, studentType, course, yearSection, tupID, password], function (err) {
+    if (err) {
+      return res.status(500).send('Failed to register user');
+    }
+    // Redirect to the root URL after successful registration
+    res.redirect('/');
+  });
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
